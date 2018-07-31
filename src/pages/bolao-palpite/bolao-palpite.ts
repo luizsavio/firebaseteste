519b5c13249c53ed85764d6606f9df01e28666fe
Luiz Savio <luizsavio2009@hotmail.com>
2018-07-31 11:03:38 -0300

    commit resolver Editar

--- src/pages/bolao-palpite/bolao-palpite.ts
+++ src/pages/bolao-palpite/bolao-palpite.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '../../../node_modules/@angular/forms';
+import { FirestoreServiceProvider } from '../../providers/firestore-service/firestore-service';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 
 /**
  * Generated class for the BolaoPalpitePage page.
@@ -14,12 +17,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'bolao-palpite.html',
 })
 export class BolaoPalpitePage {
-  public data: string = new Date().toISOString();
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  public data: string; /*= new Date().toISOString();*/
+  public palpiteForm: FormGroup;
+  public bolao: any;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad BolaoPalpitePage');
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public firestoreService: FirestoreServiceProvider,
+    public authService: AuthServiceProvider,
+    public fb: FormBuilder) {
+    this.palpiteForm = fb.group({
+      dataPalpite: ['', Validators.compose([Validators.required])]
+    });
+    this.bolao = navParams.data;
+    for (const item of this.bolao.bolaoparticipantes.participantes) {
+      if (item.idUsuario == authService.currentUser.uid) {
+        (item.dataPalpite != null) ? this.data = item.dataPalpite : '';
+      }
+    }
+  }
+  palpitar() {
+    let data = this.palpiteForm.value;
+    let lista = new Array();
+        lista = this.bolao.bolaoparticipantes.participantes;
+        for (let item of lista) {
+          if(item.idUsuario == this.authService.currentUser.uid){
+            item.dataPalpite = data.dataPalpite;
+            console.log('item: ', item);
+            
+          }
+        }
+        this.bolao.bolaoparticipantes.participantes = lista;
+        this.firestoreService.atualizarDocumento('bolaoparticipantes', this.bolao.bolaoparticipantes.idBolao, {participantes: lista})
   }
 
 }
